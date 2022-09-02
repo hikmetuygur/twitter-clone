f93fd17bcad1fb3afc1684fe6a1b50251009ba76
huygur <huygur@huygur.local>
2022-09-02 17:07:01 +0300

    Project init

---
+++ twitter-clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  twitter-clone
+//
+//  Created by huygur on 2.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ twitter-clone/twitter_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_cloneApp.swift
+//  twitter-clone
+//
+//  Created by huygur on 2.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct twitter_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
